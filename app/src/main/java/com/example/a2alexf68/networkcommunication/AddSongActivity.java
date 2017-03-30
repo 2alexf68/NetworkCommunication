28b803b4c343aa18ea6afbac75a1b7a53a873bb5
2alexf68 <2alexf68@solent.ac.uk>
2017-03-30 10:58:35 +0100

    add song not functional2

--- app/src/main/java/com/example/a2alexf68/networkcommunication/AddSongActivity.java
+++ app/src/main/java/com/example/a2alexf68/networkcommunication/AddSongActivity.java
@@ -1,13 +1,20 @@
 package com.example.a2alexf68.networkcommunication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class AddSongActivity extends Activity implements View.OnClickListener{
@@ -17,7 +24,7 @@ public class AddSongActivity extends Activity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_song);
 
-        Button addSongButton = (Button) findViewById(R.id.addSongButton);
+        Button addSongButton = (Button) findViewById(R.id.addSongButton2);
         addSongButton.setOnClickListener(this);
     }
     @Override
@@ -39,15 +46,50 @@ public class AddSongActivity extends Activity implements View.OnClickListener{
         protected String doInBackground(String... params)
         {
             String postData = "song=" + params [0] + "%artist=" +params [1] + params [2];
+            HttpURLConnection conn = null;
 
             try{
-                URL urlOject = new URL("http://www.free-map.org.uk/course/mad/ws/hits.php");
-            }
-            catch (IOException e){
-                return "Error" + e.getMessage();
-            }
+                URL urlObject = new URL("http://www.free-map.org.uk/course/mad/ws/hits.php");
+                conn = (HttpURLConnection)urlObject.openConnection();
 
-            return null;
+                conn.setDoOutput(true);
+                conn.setFixedLengthStreamingMode(postData.length());
+
+                OutputStream out = null;
+                out = conn.getOutputStream();
+                out.write(postData.getBytes());
+                if(conn.getResponseCode() == 200)
+                {
+                    InputStream in = conn.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+                    String all = "", line;
+                    while((line = br.readLine()) !=null)
+                        all += line;
+                    return all;
+                }
+                else
+                    return "HTTP ERROR: " + conn.getResponseCode();
+
+
+            }
+            catch(IOException e)
+            {
+                return e.toString();
+            }
+            finally
+            {
+                if(conn!=null)
+                    conn.disconnect();
+            }
         }
+        public void onPostExecute(String result)
+        {
+            TextView tv2= (TextView)findViewById(R.id.textView2);
+            tv2.setText(result);
+        }
+
     }
+
 }
+
+
